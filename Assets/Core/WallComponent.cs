e429e52a13dfba7eb4525450c248fd5550d7db2f
C.S. Woodward <82497926+pantagruelianist@users.noreply.github.com>
2024-08-15 21:52:50 -0500

    base class for walls and doors

---
+++ Assets/Core/WallComponent.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallComponent : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+  public abstract void Examine()
+    {
+    }
+}
---
+++ Assets/Doorway.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Doorway : WallComponent
+{
+    public bool isLocked = true; 
+    void Start()
+    {
+        
+    }
+
+
+}
